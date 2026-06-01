15aada87cc9273e6fde9c29286083001f7a44d40
Tiago Juca <tiago.juca@altoqi.com.br>
2026-06-01 20:36:55 -0300

    ajustar script

--- run.bat
+++ run.bat
@@ -1,3 +1,6 @@
 @echo off
-python .\src\montador.py .\test\prog2.asm out.bin
-python .\src\computador.py out.bin
+setlocal
+pushd "%~dp0"
+cmd /c "python .\src\montador.py .\test\prog2.asm out.bin && python -u .\src\computador.py out.bin"
+popd
+endlocal
